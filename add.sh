759659d3cd0c62b584be397c8c2160119c8d29e7
Nathan <nduckett13@gmail.com>
2020-04-08 22:35:47 +1200

    Add arguments to directly execute with values

--- add.sh
+++ add.sh
@@ -1,8 +1,59 @@
 #/bin/sh
 
-read -p "Github Personal Access Token: " PAT
-read -p "Your name for the account: " name
-read -p "Email Address for account: " email
+# Function to get parameters from cli arguments
+function get_params () {
+    # =====================================
+    # Parameter parsing for bash script
+    # =====================================
+    PARAMS=""
+    while (( "$#" )); do
+    case "$1" in
+        -pat|--personal-access-token)
+        PAT=$2
+        shift 2
+        ;;
+        -n|--name)
+        name=$2
+        shift 2
+        ;;
+        -e|--email)
+        email=$2
+        shift 2
+        ;;
+        --) # end argument parsing
+        shift
+        break
+        ;;
+        -*|--*=) # unsupported flags
+        echo "Error: Unsupported flag $1" >&2
+        exit 1
+        ;;
+        *) # preserve positional arguments
+        PARAMS="$PARAMS $1"
+        shift
+        ;;
+    esac
+    done
+    # set positional arguments in their proper place
+    eval set -- "$PARAMS"
+}
+
+get_params
+
+# =====================================
+# Check values exist otherwise set them
+# =====================================
+if [ "$PAT" == "" ]; then
+    read -p "Github Personal Access Token: " PAT
+fi
+
+if [ "$name" == "" ]; then
+    read -p "Your name for the account: " name
+fi
+
+if [ "$email" == "" ]; then
+    read -p "Email Address for account: " email
+fi
 key_path="$HOME/.ssh/id_rsa"
 
 # Generate SSH key
